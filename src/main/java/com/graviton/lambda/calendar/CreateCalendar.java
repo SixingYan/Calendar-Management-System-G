556776813d2b2c56e5772d33ed08d84347ea6503
Alfonso <plutoyem@outlook.com>
2018-11-02 14:20:27 -0400

    calendar

--- src/main/java/com/graviton/lambda/calendar/CreateCalendar.java
+++ src/main/java/com/graviton/lambda/calendar/CreateCalendar.java
@@ -1,16 +1,24 @@
 package com.graviton.lambda.calendar;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 
-public class CreateCalendar implements RequestHandler<Object, String> {
-
+public class CreateCalendar implements RequestHandler<HashMap, HashMap> {
     @Override
-    public String handleRequest(Object input, Context context) {
-        context.getLogger().log("Input: " + input);
-
-        // TODO: implement your handler
-        return "Hello from Lambda!";
+    public HashMap handleRequest(HashMap request, Context context) {
+        //context.getLogger().log("Input: " + input);
+        HashMap<String, Object> respose = new HashMap<String, Object>();
+        
+        HashMap<String, Object> status = new HashMap<String, Object>();
+        status.put("code", 0);
+        status.put("msg", "OK");
+        
+        respose.put("status", status);
+        respose.put("result", true);
+        return respose;
     }
 
 }
