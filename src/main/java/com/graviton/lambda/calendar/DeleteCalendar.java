0e570304f97502b82fe6bbcb31f0a428c4076e3a
Alfonso <plutoyem@outlook.com>
2018-11-19 20:53:52 -0500

    delete calendar

--- src/main/java/com/graviton/lambda/calendar/DeleteCalendar.java
+++ src/main/java/com/graviton/lambda/calendar/DeleteCalendar.java
@@ -1,13 +1,25 @@
 package com.graviton.lambda.calendar;
 
+import java.util.ArrayList;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.graviton.lambda.calendar.db.DBMgr;
 import com.graviton.lambda.calendar.model.*;
-
+/**
+ * 
+ * @author Yichen Li | Sixing Yan
+ */
 public class DeleteCalendar implements RequestHandler<SelectCalendar, ResultResponse> {
-
-    @Override
     public ResultResponse handleRequest(SelectCalendar cld, Context context) {
-        return new ResultResponse(true, 0, "OK");
+    	DBMgr db=new DBMgr();
+    	
+    	String name=cld.getName();
+    	ArrayList<Calendar> result = db.doDPC(name);
+    	if (result.getClass()==ArrayList.class) {
+    		return new ResultResponse(true, 0, "OK");
+    	}else {
+    		return new ResultResponse(true, 0, "error");
+    	}
     }
 }
