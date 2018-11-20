7d3ba6b6792e256dfe65aba2c8f024c4c6e1bcc7
Alfonso <plutoyem@outlook.com>
2018-11-19 20:45:27 -0500

    Data Response model

--- src/main/java/com/graviton/lambda/calendar/model/DataResponse.java
+++ src/main/java/com/graviton/lambda/calendar/model/DataResponse.java
@@ -1,16 +1,37 @@
 package com.graviton.lambda.calendar.model;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-
+import java.util.List;
+/**
+ * 
+ * @author Sixing Yan
+ *
+ */
 public class DataResponse {
 	public int code;
 	public String message;
-	public ArrayList data;
+	public List data;
 	public DataResponse () {}
-	public DataResponse (ArrayList data, int code, String message) {
+	public DataResponse (List data, int code, String message) {
 		this.code = code;
 		this.message = message;
+		this.data = data;
+	}
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public List getData() {
+		return data;
+	}
+	public void setData(List data) {
 		this.data = data;
 	}
 	
