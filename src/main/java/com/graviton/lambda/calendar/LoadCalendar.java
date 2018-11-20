e276ed54f4f8d84ffa5623076df77860fddb2d29
Alfonso <plutoyem@outlook.com>
2018-11-19 20:57:50 -0500

    load calendar

--- src/main/java/com/graviton/lambda/calendar/LoadCalendar.java
+++ src/main/java/com/graviton/lambda/calendar/LoadCalendar.java
@@ -1,23 +1,21 @@
 package com.graviton.lambda.calendar;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.graviton.lambda.calendar.db.DBMgr;
 import com.graviton.lambda.calendar.model.*;
-
+/**
+ * 
+ * @author Yichen Li | Sixing Yan
+ */
 public class LoadCalendar implements RequestHandler<SelectCalendar, DataResponse> {
-    @Override
     public DataResponse handleRequest(SelectCalendar cld, Context context) {
-    	HashMap calendar = new HashMap();
-    	calendar.put("name", cld.name);
-    	calendar.put("duration", 30);
-    	calendar.put("earlyTime", 1000);
-    	calendar.put("lateTime", 1700);
-    	calendar.put("startDate", 20180900);
-    	calendar.put("endDate", 20181214);
     	
-        return new DataResponse(new ArrayList<HashMap>(Arrays.asList(calendar)), 0, "OK");
+    	DBMgr db=new DBMgr();
+    	
+    	ArrayList<Calendar> list=db.doLPC(cld.getName());
+    	
+        return new DataResponse(list, 0, "OK");
     }
 }
